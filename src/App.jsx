220f6cb0e3ecea28a34911dbf1f86c59a6d6febf
udaypanesar <ud.panesar@gmail.com>
2024-03-09 20:23:13 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import jiraLogo from '/jira.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={jiraLogo} className="logo" alt="Jira logo" />
+        </a>
+      </div>
+      <h1>Jira Clone</h1>
+    </>
+  )
+}
+
+export default App
